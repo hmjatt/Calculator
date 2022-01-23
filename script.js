e4ec114cbf7c1400070caabb949abc2efca7c55a
Harmeet Matharoo <hmjatt@gmail.com>
2022-01-23 14:44:11 -0700

    Addition works

--- script.js
+++ script.js
@@ -1,14 +1,73 @@
-const buttons = document.querySelectorAll("button");
+const numbers = document.querySelectorAll(".num");
 const screen = document.getElementById("screen-div");
+const operator = document.querySelectorAll(".operator");
+const equals = document.getElementById("equals");
+const clear = document.querySelectorAll(".clear");
 
 
 
+let nums = [];
+let argNumber;
+let opr
+let splitUsingOp;
+let joinNums;
+let sum;
 
-buttons.forEach(function(elem) {
+
+numbers.forEach(function(elem) {
     elem.addEventListener("click", function populate() {
-        screen.style.flexwrap = "wrap";
-        // console.log(elem.innerHTML);
-        screen.innerHTML += elem.innerHTML;
+      
+        let ele = elem.innerHTML;
+        //convert input to Array
+
+        argNumber = ele;
+        
+        nums.push(argNumber);
+        screen.innerHTML += argNumber;
+        joinNums = nums.join("");
+        // console.log(argNumber, joinNums);
+    });
+});
+
+
+
+operator.forEach(function(op) {
+    op.addEventListener("click", function evaluate() {
+
+        
+
+        opr = op.innerHTML;
+
+     
+
+
+        nums.push(opr);
+
+
+        screen.innerHTML = nums.join("")
+        // console.log(joinNums, opr);
+    });
+});
+
+equals.addEventListener("click", function calculates() {
+    //    split using oerator
+       splitUsingOp = joinNums.split(opr);
+       operate();
+
+    //    console.log(splitUsingOp[0], opr);
+});
+
+
+clear.forEach(function(clears) {
+    clears.addEventListener("click", function clearIt() {
+        screen.innerHTML = "";
+        nums = [];
+        argNumber = 0;
+        opr = "";
+        splitUsingOp = 0;;
+        joinNums = 0;;
+        sum = 0;
+
     });
 });
 
@@ -16,7 +75,45 @@ const add = function(a, b) {
     return a + b;
 };
 
-console.log("add:" + add(2, 4));
+
+// console.log("add:" + add(2, 4));
+
+
+
+
+function operate(operator, num1, num2) {
+
+    operator = opr;
+    num1 = parseInt(splitUsingOp[0]);
+    num2 = parseInt(splitUsingOp[1]);
+
+    
+
+    if(operator == "+"){
+        sum = (add(num1, num2));
+        console.log(sum);
+        screen.innerHTML = "";
+        screen.innerHTML = sum;
+        return sum;
+    
+    }
+
+
+}
+
+// console.log(operate("+", 4, 4));
+
+
+
+
+
+
+
+
+
+
+
+
 
 const subtract = function(a, b) {
     return a - b;
@@ -24,9 +121,9 @@ const subtract = function(a, b) {
 
 console.log("subtract:" + subtract(4, 2));
 
-const sum = function(sumArgs) {
+const sums = function(sumArgs) {
 
-  let sum;
+//   let sum;
 
   if(sumArgs.length == 0) {
       sum = 0;
@@ -39,7 +136,7 @@ const sum = function(sumArgs) {
     return sum;
 };
 
-console.log("sum:" + sum([1, 3, 5, 7, 9]));
+console.log("sum:" + sums([1, 3, 5, 7, 9]));
 
 const multiply = function(mulArgs) {
 
@@ -72,10 +169,4 @@ const factorial = function(num) {
 console.log("fac:" + factorial(10));
 
 
-function operate(operator, num1, num2) {
-    if(operator == "+"){
-        return ("add:" + add(num1, num2));
-    }
-}
 
-console.log(operate("+", 4, 4));
